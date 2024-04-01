d1d2135fc9e609b9cc874bd3fc246a872d4a8112
Pedro-Black <Phrod2007@gmail.com>
2024-04-01 10:54:08 -0300

    Definindo rotas em urls e admin

--- app/urls.py
+++ app/urls.py
@@ -9,11 +9,14 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet
-
+from core.views import UserViewSet, CategoriaViewSet, AcessorioViewSet, CorViewSet, MarcaViewSet
 router = DefaultRouter()
 
 router.register(r"users", UserViewSet, basename="users")
+router.register(r"categorias", CategoriaViewSet, basename="categorias")
+router.register(r"cores", CorViewSet, basename="cores")
+router.register(r"acessorios", AcessorioViewSet, basename="acessorios")
+router.register(r"marcas", MarcaViewSet, basename="marcas")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
